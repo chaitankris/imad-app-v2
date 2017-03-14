01fab07e90595db94e2e620c66bbc5a0919d4d9f
Krishna Chaitanya Thottempudi <chaitanyakrishna119@gmail.com>
2017-03-14 20:42:26 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+   var interval = setinterval(moveright,50);  
+};
+
+function moveright(){
+    marginleft = marginleft + 1;
+    img.style.marginLeft = marginleft +'px';
+}
+
+var marginleft = 0;
+
